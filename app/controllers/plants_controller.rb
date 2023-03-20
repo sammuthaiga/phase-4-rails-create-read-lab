01aaf226f23c63b532fc0e40d1bb12967f70fabd
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-03-20 13:42:52 +0300

    commit tree d75f24268bbafe14ffda48c7996b31768b479e96

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,21 @@
+class PlantsController < ApplicationController
+        def index
+            render json: Plant.all
+        end
+    
+        def show
+            plant  = Plant.find_by(id: params[:id])
+            render json: plant
+        end
+    
+        def create
+            render json: Plant.create(plant_params), status: :created
+        end
+    
+        private
+    
+        def plant_params
+            params.permit(:name, :image, :price)
+        end
+    
+end
